2a2f22f25eac7ef02fd2137c10e766fc3cb800c6
Pablo Berganza <pberganza10@gmail.com>
2018-07-23 22:12:09 -0600

    FIXED: Type->Types

--- src/comments/schemas/comment.schema.ts
+++ src/comments/schemas/comment.schema.ts
@@ -1,6 +1,7 @@
 import * as mongoose from 'mongoose';
 
 export const CommentSchema = new mongoose.Schema({
+    article: { type: mongoose.Schema.Types.ObjectId, ref: 'Article' },
     text: String,
     date: Date,
     user: String,
