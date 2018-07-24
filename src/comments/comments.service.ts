fb90002612dd6e3be00e1ffe049a09828d6435c4
Pablo Berganza <pberganza10@gmail.com>
2018-07-23 21:41:23 -0600

    ADDED: Interface

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateCommentDto } from './dto/create-comment.dto';
+import { Comment } from './interfaces/comment.interface';
 
 @Injectable()
 export class CommentsService {
---
+++ src/comments/interfaces/comment.interface.ts
@@ -0,0 +1,5 @@
+export interface Comment {
+    readonly text: string;
+    readonly user: string;
+    readonly date: Date;
+}
