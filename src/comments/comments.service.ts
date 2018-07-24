7c9235910b5adc54a1b92424cc7cbfff75901c1d
Pablo Berganza <pberganza10@gmail.com>
2018-07-23 21:38:30 -0600

    ADDED: methods

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateCommentDto } from './dto/create-comment.dto';
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+    constructor(@InjectModel('Comment') private readonly commentModel: Model<Comment>) { }
+
+    async create(createCommentDto: CreateCommentDto): Promise<Comment> {
+        const createdComment = new this.commentModel(createCommentDto);
+        return await createdComment.save();
+    }
+
+    async findAll(): Promise<Comment[]> {
+        return await this.commentModel.find().exec();
+    }
+}
