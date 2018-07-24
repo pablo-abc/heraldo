33d4faf9b1a2337166bbc6a640bbe16c1efff935
Pablo Berganza <pberganza10@gmail.com>
2018-07-23 21:36:42 -0600

    REMOVED: CommentsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CommentsModule } from './comments/comments.module';
 const { DB_NAME: dbname, DB_USER: dbuser, DB_HOST: dbhost, DB_PORT: dbport } = process.env;
 const dburi = `mongodb://${dbuser}@${dbhost}:${dbport}/${dbname}`;
 
 @Module({
-    imports: [ArticlesModule, MongooseModule.forRoot(dburi), CommentsModule],
+    imports: [ArticlesModule, MongooseModule.forRoot(dburi)],
     controllers: [AppController],
     providers: [AppService],
 })
