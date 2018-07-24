c9fac494c73627dc992935ed030b986c8278ec71
Pablo Berganza <pberganza10@gmail.com>
2018-07-23 21:37:56 -0600

    ADDED: CommentsModule

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -3,9 +3,10 @@ import { ArticlesController } from './articles.controller';
 import { ArticlesService } from './articles.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ArticleSchema } from './schemas/article.schema';
+import { CommentsModule } from '../comments/comments.module';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: 'Article', schema: ArticleSchema }])],
+    imports: [MongooseModule.forFeature([{ name: 'Article', schema: ArticleSchema }]), CommentsModule],
     controllers: [ArticlesController],
     providers: [ArticlesService],
 })
