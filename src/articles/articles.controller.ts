b925abba9c84fcd698db155ee4303477a16250f1
Pablo Berganza <pberganza10@gmail.com>
2018-07-23 22:10:11 -0600

    ADDED: CommentsService

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -2,10 +2,16 @@ import { Controller, Get, Post, Delete, Body, Param, HttpCode } from '@nestjs/co
 import { CreateArticleDto } from './dto/create-article.dto';
 import { ArticlesService } from './articles.service';
 import { Article } from './interfaces/article.interface';
+import { CommentsService } from '../comments/comments.service';
+import { CreateCommentDto } from '../comments/dto/create-comment.dto';
+import { Comment } from '../comments/interfaces/comment.interface';
 
 @Controller('articles')
 export class ArticlesController {
-    constructor(private readonly articlesService: ArticlesService) { }
+    constructor(
+        private readonly articlesService: ArticlesService,
+        private readonly commentsService: CommentsService,
+    ) { }
 
     @Get()
     findAll(): Promise<Article[]> {
@@ -20,6 +26,11 @@ export class ArticlesController {
     @Post()
     create(@Body() createArticleDto: CreateArticleDto): Promise<Article> {
         return this.articlesService.create(createArticleDto);
+    }
+
+    @Post(':id/comments')
+    addComment(@Body() createCommentDto: CreateCommentDto): Promise<Comment> {
+        return this.commentsService.create(createCommentDto);
     }
 
     @Delete(':id')
