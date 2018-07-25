a0ec34ab3fd8ae9e50cf95f74c1000bef18d80c6
Pablo Berganza <pberganza10@gmail.com>
2018-07-25 16:18:38 -0600

    ADDED: Default value for date

--- src/articles/schemas/article.schema.ts
+++ src/articles/schemas/article.schema.ts
@@ -2,7 +2,7 @@ import * as mongoose from 'mongoose';
 
 export const ArticleSchema = new mongoose.Schema({
     title: String,
-    date: Date,
+    date: { type: Date, default: Date.now },
     text: String,
     user: String,
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
