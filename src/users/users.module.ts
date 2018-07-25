44c2df18982738668a1af729018393eed053e779
Pablo Berganza <pberganza10@gmail.com>
2018-07-25 17:16:49 -0600

    ADDED: Mongoose dependencies and create endpoint

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './schemas/user.schema';
+import { RolesModule } from '../roles/roles.module';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService]
+    imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]), RolesModule],
+    controllers: [UsersController],
+    providers: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule { }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './interfaces/user.interface';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectModel('User') private readonly userModel: Model<User>) { }
+
+    async create(createUserDto: CreateUserDto): Promise<User> {
+        const createdUser = new this.userModel(createUserDto);
+        return createdUser.save();
+    }
+}
