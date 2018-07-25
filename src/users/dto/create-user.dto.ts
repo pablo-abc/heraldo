75b2c98eb5d1ddce212abaac085e75f98bd2d63d
Pablo Berganza <pberganza10@gmail.com>
2018-07-25 17:58:26 -0600

    ADDED: Methods

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsEmail } from 'class-validator';
+import { IsString, IsEmail, IsEmpty } from 'class-validator';
 
 export class CreateUserDto {
     @IsString()
@@ -9,4 +9,24 @@ export class CreateUserDto {
 
     @IsEmail()
     readonly email: string;
+
+    @IsEmpty()
+    readonly created: Date;
+
+    @IsEmpty()
+    readonly modified: Date;
+
+    constructor(username: string, password: string, email: string) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    storePassword(createUserDto: CreateUserDto, password: string): CreateUserDto {
+        return new CreateUserDto(
+            createUserDto.username,
+            password,
+            createUserDto.email,
+        );
+    }
 }
