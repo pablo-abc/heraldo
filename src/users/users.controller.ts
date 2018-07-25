99ba27e9f6daa14afda5453c332bcbf4e0d0dcc8
Pablo Berganza <pberganza10@gmail.com>
2018-07-25 17:58:39 -0600

    ADDED: Create user route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UsePipes, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { HashPasswordPipe } from '../pipes/hash-password.pipe';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './interfaces/user.interface';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) { }
+
+    @Post()
+    @UsePipes(new HashPasswordPipe())
+    create(@Body() createUserDto: CreateUserDto): Promise<User> {
+        return this.usersService.create(createUserDto);
+    }
+}
