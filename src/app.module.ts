31a34e454eb9a5314928aa23d66e4c7d3ccaf831
Pablo Berganza <pberganza10@gmail.com>
2018-07-25 16:18:54 -0600

    ADDED: users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 const { DB_NAME: dbname, DB_USER: dbuser, DB_HOST: dbhost, DB_PORT: dbport } = process.env;
 const dburi = `mongodb://${dbhost}:${dbport}/${dbname}`;
 
 @Module({
-    imports: [ArticlesModule, MongooseModule.forRoot(dburi)],
+    imports: [ArticlesModule, MongooseModule.forRoot(dburi), UsersModule],
     controllers: [AppController],
     providers: [AppService],
 })
