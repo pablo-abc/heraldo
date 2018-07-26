d78757daeefc05e5a7028a63b8ceec12ecab95c8
Pablo Berganza <pberganza10@gmail.com>
2018-07-26 11:30:02 -0600

    ADDED: RoleMappingModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,14 @@ import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './schemas/user.schema';
 import { RolesModule } from '../roles/roles.module';
+import { RoleMappingsModule } from '../role-mappings/role-mappings.module';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]), RolesModule],
+    imports: [
+        MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+        RolesModule,
+        RoleMappingsModule,
+    ],
     controllers: [UsersController],
     providers: [UsersService],
 })
