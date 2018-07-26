9a7ce6217e57f57259b9b992362ea668069dde96
Pablo Berganza <pberganza10@gmail.com>
2018-07-26 13:45:17 -0600

    CHANGED: User to CreateUserDto

--- src/pipes/hash-password.pipe.ts
+++ src/pipes/hash-password.pipe.ts
@@ -1,16 +1,16 @@
 import { PipeTransform, Injectable, ArgumentMetadata, HttpStatus } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from '../users/dto/create-user.dto';
-import { User } from '../users/interfaces/user.interface';
 
 @Injectable()
-export class HashPasswordPipe implements PipeTransform<CreateUserDto, Promise<User>> {
-    async transform(user: User, metadata: ArgumentMetadata): Promise<User> {
+export class HashPasswordPipe implements PipeTransform<CreateUserDto, Promise<CreateUserDto>> {
+    async transform(user: CreateUserDto, metadata: ArgumentMetadata): Promise<CreateUserDto> {
         const hashedPassword = await bcrypt.hash(user.password, 12);
         return new CreateUserDto(
             user.username,
             hashedPassword,
             user.email,
+            user.role,
         );
     }
 }
