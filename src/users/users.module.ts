01c926705766809e6fce51f8c87d50c9638de077
Pablo Berganza <pberganza10@gmail.com>
2018-07-26 15:24:12 -0600

    ADDED: Access Token schema

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -7,6 +7,7 @@ import { RolesModule } from '../roles/roles.module';
 import { RoleMappingsModule } from '../role-mappings/role-mappings.module';
 import { RoleMappingSchema } from 'role-mappings/schemas/role-mapping.schema';
 import { RoleSchema } from 'roles/schemas/role.schema';
+import { AccessTokenSchema } from './schemas/user-access-token.schema';
 
 @Module({
     imports: [
@@ -14,6 +15,7 @@ import { RoleSchema } from 'roles/schemas/role.schema';
             { name: 'User', schema: UserSchema },
             { name: 'RoleMapping', schema: RoleMappingSchema },
             { name: 'Role', schema: RoleSchema },
+            { name: 'AccessToken', schema: AccessTokenSchema },
         ]),
         RolesModule,
         RoleMappingsModule,
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,16 @@
-import { Injectable, BadRequestException, ForbiddenException, NotFoundException } from '@nestjs/common';
+import { Injectable, BadRequestException, ForbiddenException, NotFoundException, ConflictException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './interfaces/user.interface';
 import { RoleMapping } from '../role-mappings/interfaces/role-mapping.interface';
 import { Role } from '../roles/interfaces/role.interface';
+import { AccessToken } from './interfaces/user-access-token.interface';
 import { CreateUserDto } from './dto/create-user.dto';
 import { CreateRoleMappingDto } from '../role-mappings/dto/create-role-mapping.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import * as jwt from 'jsonwebtoken';
 import * as bcrypt from 'bcrypt';
+import * as uuidv4 from 'uuid/v4';
 
 @Injectable()
 export class UsersService {
@@ -16,9 +18,17 @@ export class UsersService {
         @InjectModel('User') private readonly userModel: Model<User>,
         @InjectModel('RoleMapping') private readonly roleMappingModel: Model<RoleMapping>,
         @InjectModel('Role') private readonly roleModel: Model<Role>,
+        @InjectModel('AccessToken') private readonly accessTokenModel: Model<AccessToken>,
     ) { }
 
     async create(createUserDto: CreateUserDto): Promise<User> {
+        const foundUser = await this.userModel.findOne({
+            $or: [
+                { username: createUserDto.username },
+                { email: createUserDto.email },
+            ],
+        });
+        if (foundUser) throw new ConflictException('Email or username already in use');
         const createdUser = new this.userModel(createUserDto);
         const role = await this.roleModel.findOne({ name: createUserDto.role || 'user' });
         if (!role) throw new NotFoundException('Role does not exist');
@@ -40,8 +50,19 @@ export class UsersService {
         if (!user) throw new BadRequestException();
         if (!await bcrypt.compare(loginUserDto.password, user.password))
             throw new BadRequestException();
+        const userRoles = await this.roleMappingModel.find({ userId: user._id }).populate('roleId', '-_id name').exec();
+        const parsedRoles = userRoles.map(role => {
+            return role.roleId.name;
+        });
+        const createdAccessToken = new this.accessTokenModel({
+            userId: user._id,
+            jwtid: uuidv4(),
+        });
+        const accessToken = await createdAccessToken.save();
         return jwt.sign({
             _id: user._id,
+            jwtid: accessToken.jwtid,
+            roles: parsedRoles,
         }, process.env.SECRET, { expiresIn: '1w' });
     }
 }
