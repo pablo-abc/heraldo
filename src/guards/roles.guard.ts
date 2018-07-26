89b0e75f08253cb4c267cbdf3094ba43c4379200
Pablo Berganza <pberganza10@gmail.com>
2018-07-26 10:56:35 -0600

    ADDED: Reflector metadata

--- src/guards/roles.guard.ts
+++ src/guards/roles.guard.ts
@@ -1,10 +1,27 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
+    constructor(private readonly reflector: Reflector) { }
+
     canActivate(
         context: ExecutionContext,
     ): Promise<boolean> | boolean {
+        const roles = this.reflector.get<string[]>('roles', context.getHandler());
+        if (!roles)
+            return true;
+        const request = context.switchToHttp().getRequest();
+        if (!request.headers.authorization)
+            return false;
+        const token = request.headers.authorization;
+        try {
+            const user = jwt.verify(token, process.env.SECRET);
+        }
+        catch (err) {
+            return false;
+        }
         return true;
     }
 }
