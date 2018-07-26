0a61e47f1e915798f3eefd6c0d58e495f90b91bb
Pablo Berganza <pberganza10@gmail.com>
2018-07-26 13:46:00 -0600

    ADDED: role

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsEmail, IsEmpty } from 'class-validator';
+import { IsString, IsEmail, IsEmpty, IsOptional } from 'class-validator';
 
 export class CreateUserDto {
     @IsString()
@@ -10,15 +10,20 @@ export class CreateUserDto {
     @IsEmail()
     readonly email: string;
 
+    @IsOptional()
+    @IsString()
+    readonly role?: string;
+
     @IsEmpty()
     readonly created: Date;
 
     @IsEmpty()
     readonly modified: Date;
 
-    constructor(username: string, password: string, email: string) {
+    constructor(username: string, password: string, email: string, role?: string) {
         this.username = username;
         this.password = password;
         this.email = email;
+        if (role) this.role = role;
     }
 }
