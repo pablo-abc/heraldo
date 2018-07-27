05ff63cdb41401659d0d3ecf9b4a2b680cdefb6a
Pablo Berganza <pberganza10@gmail.com>
2018-07-27 17:44:31 -0600

    pipes: indent and role->roles

--- src/pipes/hash-password.pipe.ts
+++ src/pipes/hash-password.pipe.ts
@@ -4,13 +4,13 @@ import { CreateUserDto } from '../users/dto/create-user.dto';
 
 @Injectable()
 export class HashPasswordPipe implements PipeTransform<CreateUserDto, Promise<CreateUserDto>> {
-    async transform(user: CreateUserDto, metadata: ArgumentMetadata): Promise<CreateUserDto> {
-        const hashedPassword = await bcrypt.hash(user.password, 12);
-        return new CreateUserDto(
-            user.username,
-            hashedPassword,
-            user.email,
-            user.role,
-        );
-    }
+  async transform(user: CreateUserDto, metadata: ArgumentMetadata): Promise<CreateUserDto> {
+    const hashedPassword = await bcrypt.hash(user.password, 12);
+    return new CreateUserDto(
+      user.username,
+      hashedPassword,
+      user.email,
+      user.roles,
+    );
+  }
 }
