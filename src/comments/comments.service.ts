873c2095508e76d431afca9551fec0eb9ff6bae8
Pablo Berganza <pberganza10@gmail.com>
2018-07-27 13:47:10 -0600

    ADDED: Formatting and jwt

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -3,19 +3,21 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { Comment } from './interfaces/comment.interface';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class CommentsService {
-    constructor(@InjectModel('Comment') private readonly commentModel: Model<Comment>) { }
+  constructor(@InjectModel('Comment') private readonly commentModel: Model<Comment>) { }
 
-    async create(createCommentDto: CreateCommentDto, article?: string): Promise<Comment> {
-        const createdComment = article ?
-            new this.commentModel(createCommentDto.setArticle(article)) :
-            new this.commentModel(createCommentDto);
-        return await createdComment.save();
-    }
+  async create(createCommentDto: CreateCommentDto, req, article?: string): Promise<Comment> {
 
-    async findAll(): Promise<Comment[]> {
-        return await this.commentModel.find().exec();
-    }
+    const createdComment = article ?
+      new this.commentModel(createCommentDto.setArticle(article)) :
+      new this.commentModel(createCommentDto);
+    return await createdComment.save();
+  }
+
+  async findAll(): Promise<Comment[]> {
+    return await this.commentModel.find().exec();
+  }
 }
--- src/comments/schemas/comment.schema.ts
+++ src/comments/schemas/comment.schema.ts
@@ -1,8 +1,16 @@
 import * as mongoose from 'mongoose';
+const Schema = mongoose.Schema;
 
-export const CommentSchema = new mongoose.Schema({
-    article: { type: mongoose.Schema.Types.ObjectId, ref: 'Article' },
-    text: String,
-    date: Date,
-    user: String,
+export const CommentSchema = new Schema({
+  article: {
+    type: Schema.Types.ObjectId,
+    ref: 'Article',
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  text: String,
+  date: Date,
+  user: String,
 });
