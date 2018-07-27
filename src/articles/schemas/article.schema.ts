38db816aeb86a78d22f0200c6745aec489fc571c
Pablo Berganza <pberganza10@gmail.com>
2018-07-27 11:46:24 -0600

    MODIFIED: created and stuff

--- src/articles/schemas/article.schema.ts
+++ src/articles/schemas/article.schema.ts
@@ -2,9 +2,27 @@ import * as mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 export const ArticleSchema = new Schema({
-    title: String,
-    date: { type: Date, default: Date.now },
-    text: String,
-    user: String,
-    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+  title: String,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  text: String,
+  user: String,
+  comments: [
+    { type: Schema.Types.ObjectId, ref: 'Comment' },
+  ],
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  modified: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+ArticleSchema.pre('save', function(next) {
+  this.modified = new Date();
+  next();
 });
