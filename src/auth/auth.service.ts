1be508e6f7b0e43df9dae4e73ca4c6fbc417ccf6
Pablo Berganza <pberganza10@gmail.com>
2018-07-27 17:44:19 -0600

    Indent and role->roles

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,14 +4,14 @@ import { CreateUserDto } from '../users/dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
-    validateCreateUser(req, createUserDto: CreateUserDto): boolean {
-        try {
-            if (!createUserDto.role) return true;
-            const token = req.headers.authorization.split(' ')[1];
-            const user = jwt.decode(token);
-            return user.roles.includes('admin');
-        } catch (err) {
-            return false;
-        }
+  validateCreateUser(req, createUserDto: CreateUserDto): boolean {
+    try {
+      if (!createUserDto.roles) return true;
+      const token = req.headers.authorization.split(' ')[1];
+      const user = jwt.decode(token);
+      return user.roles.includes('admin');
+    } catch (err) {
+      return false;
     }
+  }
 }
