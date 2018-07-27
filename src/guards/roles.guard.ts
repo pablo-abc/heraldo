072fd676696ce45d7e7f00d4df642ab363ef9d5a
Pablo Berganza <pberganza10@gmail.com>
2018-07-27 15:05:29 -0600

    Indentation

--- src/guards/roles.guard.ts
+++ src/guards/roles.guard.ts
@@ -4,24 +4,24 @@ import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-    constructor(private readonly reflector: Reflector) { }
+  constructor(private readonly reflector: Reflector) { }
 
-    canActivate(
-        context: ExecutionContext,
-    ): Promise<boolean> | boolean {
-        const roles = this.reflector.get<string[]>('roles', context.getHandler());
-        if (!roles)
-            return true;
-        const request = context.switchToHttp().getRequest();
-        if (!request.headers.authorization)
-            return false;
-        const token = request.headers.authorization.split(' ')[1];
-        try {
-            const user = jwt.verify(token, process.env.SECRET);
-            return roles.some(role => user.roles.includes(role));
-        }
-        catch (err) {
-            return false;
-        }
+  canActivate(
+    context: ExecutionContext,
+  ): Promise<boolean> | boolean {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+    if (!roles)
+      return true;
+    const request = context.switchToHttp().getRequest();
+    if (!request.headers.authorization)
+      return false;
+    const token = request.headers.authorization.split(' ')[1];
+    try {
+      const user = jwt.verify(token, process.env.SECRET);
+      return roles.some(role => user.roles.includes(role));
     }
+    catch (err) {
+      return false;
+    }
+  }
 }
