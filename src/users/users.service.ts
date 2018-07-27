2833e31245656627c7cfb0b5a07318f49af547d1
Pablo Berganza <pberganza10@gmail.com>
2018-07-27 11:00:13 -0600

    IndentSize 2

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -15,63 +15,63 @@ import { ResponseToken } from 'auth/interfaces/response-token.interface';
 
 @Injectable()
 export class UsersService {
-    constructor(
-        @InjectModel('User') private readonly userModel: Model<User>,
-        @InjectModel('RoleMapping') private readonly roleMappingModel: Model<RoleMapping>,
-        @InjectModel('Role') private readonly roleModel: Model<Role>,
-        @InjectModel('AccessToken') private readonly accessTokenModel: Model<AccessToken>,
-    ) { }
+  constructor(
+    @InjectModel('User') private readonly userModel: Model<User>,
+    @InjectModel('RoleMapping') private readonly roleMappingModel: Model<RoleMapping>,
+    @InjectModel('Role') private readonly roleModel: Model<Role>,
+    @InjectModel('AccessToken') private readonly accessTokenModel: Model<AccessToken>,
+  ) { }
 
-    async create(createUserDto: CreateUserDto): Promise<User> {
-        const foundUser = await this.userModel.findOne({
-            $or: [
-                { username: createUserDto.username },
-                { email: createUserDto.email },
-            ],
-        });
-        if (foundUser) throw new ConflictException('Email or username already in use');
-        const createdUser = new this.userModel(createUserDto);
-        const role = await this.roleModel.findOne({ name: createUserDto.role || 'user' });
-        const userRole = role.name !== 'user' ? await this.roleModel.findOne({ name: 'user' }) : role;
-        if (!role || !userRole) throw new NotFoundException('Role does not exist');
-        const user = await createdUser.save();
-        const createRoleMappingDto = new CreateRoleMappingDto(user._id, role._id);
-        const createdRoleMapping = new this.roleMappingModel(createRoleMappingDto);
-        const roleMapping = await createdRoleMapping.save();
-        const { password, ...rest } = user.toJSON();
-        if (role.name === 'user')
-            return rest;
-    }
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const foundUser = await this.userModel.findOne({
+      $or: [
+        { username: createUserDto.username },
+        { email: createUserDto.email },
+      ],
+    });
+    if (foundUser) throw new ConflictException('Email or username already in use');
+    const createdUser = new this.userModel(createUserDto);
+    const role = await this.roleModel.findOne({ name: createUserDto.role || 'user' });
+    const userRole = role.name !== 'user' ? await this.roleModel.findOne({ name: 'user' }) : role;
+    if (!role || !userRole) throw new NotFoundException('Role does not exist');
+    const user = await createdUser.save();
+    const createRoleMappingDto = new CreateRoleMappingDto(user._id, role._id);
+    const createdRoleMapping = new this.roleMappingModel(createRoleMappingDto);
+    const roleMapping = await createdRoleMapping.save();
+    const { password, ...rest } = user.toJSON();
+    if (role.name === 'user')
+      return rest;
+  }
 
-    async login(loginUserDto: LoginUserDto, roles?: string[]): Promise<ResponseToken> {
-        const user: User = await this.userModel.findOne({
-            $or: [
-                { username: loginUserDto.username },
-                { email: loginUserDto.email },
-            ],
-        }).exec();
-        if (!user) throw new BadRequestException();
-        if (!await bcrypt.compare(loginUserDto.password, user.password))
-            throw new BadRequestException();
-        const userRoles = await this.roleMappingModel.find({ userId: user._id }).populate('roleId', '-_id name').exec();
-        const parsedRoles = userRoles.map(role => {
-            return role.roleId.name;
-        });
-        const createdAccessToken = new this.accessTokenModel({
-            userId: user._id,
-            jwtid: uuidv4(),
-        });
-        const accessToken = await createdAccessToken.save();
-        const token = jwt.sign({
-            _id: user._id,
-            jwtid: accessToken.jwtid,
-            roles: parsedRoles,
-        }, process.env.SECRET, { expiresIn: 3600 });
-        const responseToken = {
-            access_token: token,
-            expires_in: 3600,
-            token_type: 'Bearer',
-        };
-        return responseToken;
-    }
+  async login(loginUserDto: LoginUserDto, roles?: string[]): Promise<ResponseToken> {
+    const user: User = await this.userModel.findOne({
+      $or: [
+        { username: loginUserDto.username },
+        { email: loginUserDto.email },
+      ],
+    }).exec();
+    if (!user) throw new BadRequestException();
+    if (!await bcrypt.compare(loginUserDto.password, user.password))
+      throw new BadRequestException();
+    const userRoles = await this.roleMappingModel.find({ userId: user._id }).populate('roleId', '-_id name').exec();
+    const parsedRoles = userRoles.map(role => {
+      return role.roleId.name;
+    });
+    const createdAccessToken = new this.accessTokenModel({
+      userId: user._id,
+      jwtid: uuidv4(),
+    });
+    const accessToken = await createdAccessToken.save();
+    const token = jwt.sign({
+      _id: user._id,
+      jwtid: accessToken.jwtid,
+      roles: parsedRoles,
+    }, process.env.SECRET, { expiresIn: 3600 });
+    const responseToken = {
+      access_token: token,
+      expires_in: 3600,
+      token_type: 'Bearer',
+    };
+    return responseToken;
+  }
 }
