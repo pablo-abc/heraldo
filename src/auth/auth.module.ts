bc16a1f36d1a17a698090be83d1c8de1021df659
Pablo Berganza <pberganza10@gmail.com>
2018-07-30 22:11:31 -0600

    Auth controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AccessTokenSchema } from './schemas/user-access-token.schema';
+import { AuthController } from './auth.controller';
 
 @Module({
     imports: [MongooseModule.forFeature([{ name: 'AccessToken', schema: AccessTokenSchema }])],
@@ -10,5 +11,6 @@ import { AccessTokenSchema } from './schemas/user-access-token.schema';
         AuthService,
         MongooseModule.forFeature([{ name: 'AccessToken', schema: AccessTokenSchema }]),
     ],
+    controllers: [AuthController],
 })
 export class AuthModule { }
