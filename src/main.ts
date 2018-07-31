528b783f48e5041a90f67e5ca48d3ebc51b5dc47
Pablo Berganza <pberganza10@gmail.com>
2018-07-31 14:01:15 -0600

    Added host

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter());
   app.setGlobalPrefix('api');
   app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
